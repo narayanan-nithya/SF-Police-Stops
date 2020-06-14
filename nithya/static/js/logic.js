1e87ab38d62174118683f0a7b69b98a6a37d7d9d
Nithya Narayanan <nithyanarayanan@viveks-air.lan>
2020-06-13 21:23:09 -0700

    updated js.

--- nithya/static/js/logic.js
+++ nithya/static/js/logic.js
@@ -1,30 +1,47 @@
-var url = "http://127.0.0.1:5000/read-data";
 
-d3.json(url, function (json) {
+function loadData(race) {
+  var results = []
   
-  console.log(json);
-  
-  var arrests = []
-  
-  var noArrests = []
-  
-  for (var i = 0; i < json.length; i++) {
-  
-      if (json[i].arrest_made == true) {
-        arrests.push(json[i].arrest_made);
-      }
-        else {
-          noArrests.push(json[i].arrest_made);
-        }
-      };
-  
-  var totalArrests = arrests.length
-  
-  var free = noArrests.length
-  
-  console.log(totalArrests);
-  
-  console.log(free);
+  d3.json("test.json",function(data) {
+      var filteredData = data.filter(incident => incident.subject_race === race)
 
-});
+      filteredData.forEach(element => {
+          var result = {}
+          result["coordinates"] = [element.lat, element.lng]
+          result["age"] = element.subject_age
+          result["sex"] = element.subject_sex
+          result["arrest"] = element.arrest_made
+            
+          results.push(result);
+      });
+    
+  });
+  return results;
+}
+  
+  var hispanicData = loadData("hispanic");
+  var blackData = loadData("black");
+  var whiteData = loadData("white");
+  var asianData = loadData("asian/pacific islander");
 
+  console.log(hispanicData)
+  
+  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox/streets-v11",
+      accessToken: API_KEY
+    });
+
+  var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5,
+    layers:[streetmap]
+  });
+
+  for (var i = 0; i < hispanicData.length; i++) {
+    L.marker(hispanicData.coordinates)
+   .addTo(myMap);
+  }
+  
+  
