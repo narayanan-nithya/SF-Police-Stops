35d078f7a7e95593cb216161bf76b152c1d45f4a
Nithya Narayanan <nithyanarayanan@viveks-air.lan>
2020-06-14 01:24:19 -0700

    working files.

--- nithya/static/js/logic.js
+++ nithya/static/js/logic.js
@@ -1,47 +1,109 @@
 
-function loadData(race) {
-  var results = []
-  
-  d3.json("test.json",function(data) {
-      var filteredData = data.filter(incident => incident.subject_race === race)
+var results = []
 
-      filteredData.forEach(element => {
-          var result = {}
-          result["coordinates"] = [element.lat, element.lng]
-          result["age"] = element.subject_age
-          result["sex"] = element.subject_sex
-          result["arrest"] = element.arrest_made
-            
-          results.push(result);
-      });
-    
+d3.json("test.json" ,function(data) {
+  // Loop through mongodb data; combine 'lat'/'lng' columns into coordinates; retrieve other data.
+  data.forEach(element =>  {
+        var result = {}
+        result["coordinates"] = [element.lat, element.lng]
+        result["age"] = element.subject_age
+        result["sex"] = element.subject_sex
+        result["race"] = element.subject_race
+        result["arrest"] = element.arrest_made
+        results.push(result);
   });
-  return results;
+    // create function to return data on hispanic individuals only.
+    function hispanic(person) {
+      return person.race == "hispanic";
+  }
+    function black(person) {
+      return person.race == "black";
 }
+    function white(person) {
+      return person.race == "white";
+}
+  function asian(person) {
+      return person.race == "asian/pacific islander";
+}
+  var hispanicData = results.filter(hispanic);
+    console.log(hispanicData);
+  var blackData = results.filter(black);
+    console.log(blackData);
+  var whiteData = results.filter(white);
+    console.log(whiteData);
+  var asianData = results.filter(asian)
   
-  var hispanicData = loadData("hispanic");
-  var blackData = loadData("black");
-  var whiteData = loadData("white");
-  var asianData = loadData("asian/pacific islander");
+  // create function to return data on arrested hispanic individuals only.
+  function arrestedHispanic(person) {
+    return person.arrest == true;
+  }
+  function arrestedBlack(person) {
+    return person.arrest == true;
+  }
 
-  console.log(hispanicData)
-  
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-      maxZoom: 18,
-      id: "mapbox/streets-v11",
-      accessToken: API_KEY
-    });
+  function arrestedWhite(person) {
+    return person.arrest == true;
+  }
+
+  function arrestedAsian(person) {
+    return person.arrest == true;
+  }
+  // Pass through results array and store hispanic data in variable
+  var arrestedH = hispanicData.filter(arrestedHispanic);
+
+  var arrestedB = blackData.filter(arrestedBlack);
+
+  var arrestedW = whiteData.filter(arrestedWhite);
+
+  var arrestedA = asianData.filter(arrestedAsian);
 
   var myMap = L.map("map", {
     center: [37.09, -95.71],
-    zoom: 5,
-    layers:[streetmap]
+    zoom: 5
   });
 
-  for (var i = 0; i < hispanicData.length; i++) {
-    L.marker(hispanicData.coordinates)
-   .addTo(myMap);
+ 
+
+  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/light-v10',
+      accessToken: API_KEY
+    }).addTo(myMap);
+
+    var redMarker = L.ExtraMarkers.icon({
+      icon: 'fa-coffee',
+      markerColor: 'red',
+      shape: 'square',
+      prefix: 'fa'
+    });
+
+  for (var i = 0; i < arrestedH.length; i++) {
+    var location = arrestedH[i];
+        L.marker(location.coordinates)
+        .addTo(myMap).bindPopup("<h3>Age: "+ location.age +"</h3><h3>Sex: " + location.sex +
+        "</h3><hr><h3>Arrest: " + location.arrest + "</h3><hr><h3>Race:" + location.race + "</h3>");
   }
-  
-  
+  for (var i = 0; i < arrestedB.length; i++) {
+    var location = arrestedB[i];
+        L.marker(location.coordinates)
+        .bindPopup("<h3>Age: "+ location.age +"</h3><h3>Sex: " + location.sex +
+        "</h3><hr><h3>Arrest: " + location.arrest + "</h3><hr><h3>Race:" + location.race + "</h3>")
+          .addTo(myMap);
+  }
+  for (var i = 0; i < arrestedW.length; i++) {
+    var location = arrestedW[i];
+        L.marker(location.coordinates,{
+          icon: redMarker})
+          .addTo(myMap).bindPopup("<h3>Age: "+ location.age +"</h3><h3>Sex: " + location.sex +
+          "</h3><hr><h3>Arrest: " + location.arrest + "</h3><hr><h3>Race:" + location.race + "</h3>");
+  }
+  for (var i = 0; i < arrestedA.length; i++) {
+    var location = arrestedA[i];
+        L.marker(location.coordinates)
+        .bindPopup("<h3>Age: "+ location.age +"</h3><h3>Sex: " + location.sex +
+        "</h3><hr><h3>Arrest: " + location.arrest + "</h3><hr><h3>Race:" + location.race + "</h3>")
+          .addTo(myMap);
+  }
+
+});
